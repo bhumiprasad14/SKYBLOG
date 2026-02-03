a560fa007fe165b3869b31045223ad2675bfd7b0
Bhumi Prasad <prasad14bhumi@gmail.com>
2026-02-04 00:07:17 +0530

    commit backend

---
+++ backend/server/server.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config();
+const PORT=process.env.PORT||5000;
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDb connected");
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log("Error connecting to MongoDB:",error);
+});
