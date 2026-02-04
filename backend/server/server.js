0e3b846725e9f1e7cea2dfa58b0f817b51c76015
Bhumi Prasad <prasad14bhumi@gmail.com>
2026-02-05 00:20:35 +0530

    commit backend

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
-
 dotenv.config();
 const PORT=process.env.PORT||5000;
 mongoose.connect(process.env.MONGO_URI)
